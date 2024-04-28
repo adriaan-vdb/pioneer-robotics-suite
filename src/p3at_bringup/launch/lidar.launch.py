91532bd182e3a381a712b8380beed6de547f3e84
letanh215 <letanh215@gmail.com>
2024-04-29 01:07:09 +0800

    Added Lidar

---
+++ src/p3at_bringup/launch/lidar.launch.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+
+    sick_scan_prefix = get_package_share_directory("sick_scan_xd")
+    launch_file = "sick_tim_7xxS.launch"
+    launch_file_path = os.path.join(sick_scan_prefix, "launch", launch_file)
+    config_file = os.path.join(get_package_share_directory("p3at_bringup"), "config", "lidar.yaml")
+    # argument = [launch_file_path]
+
+    launch_lidar = Node(
+        package="sick_scan_xd",
+        executable="sick_generic_caller",
+        name="lidar",
+        parameters=[
+            config_file
+        ],
+        remappings=[
+            ("cloud", "/lidar/cloud"),
+            ("/sick_tim_7xxS/scan", "/lidar/scan")
+        ],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        launch_lidar
+    ])
